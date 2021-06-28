0617fa371f0cbeaa81441e367e76669380d8aa29
angieVa <39042313+angieVa@users.noreply.github.com>
2021-06-28 13:45:25 -0500

    Module 11 (#11)

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -16,14 +16,9 @@ describe('Buy a t-shirt', () => {
   const bankPayment: BankPayment = new BankPayment();
   const orderSummary: OrderSummary = new OrderSummary();
 
-  beforeEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
-  });
-
   it('then should be bought a t-shirt', async () => {
     browser.waitForAngularEnabled(false);
     await browser.get('http://automationpractice.com/');
-    await (browser.sleep(10000));
     await menuContentPage.goToTShirtMenu();
     await (browser.sleep(3000));
     await productListPage.goToAddToCardMenu();
