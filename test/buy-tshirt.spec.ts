de870137ec0c86e6fa6d6d4c921d57500bcc6471
Angie Valentina Córdoba <angievalentina1526@gmail.com>
2021-06-29 10:35:02 -0500

    Module 15

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -4,35 +4,57 @@ import {
   ShippingStep, PaymentStep, BankPayment, OrderSummary,
 } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModal: ProductAddedModal = new ProductAddedModal();
-  const summaryStep: SummaryStep = new SummaryStep();
-  const signInStep: SignInStep = new SignInStep();
-  const addressStep: AddressStep = new AddressStep();
-  const shippingStep: ShippingStep = new ShippingStep();
-  const paymentStep: PaymentStep = new PaymentStep();
-  const bankPayment: BankPayment = new BankPayment();
-  const orderSummary: OrderSummary = new OrderSummary();
-
-  it('then should be bought a t-shirt', async () => {
+describe('Go to Shopping page', () => {
+  beforeAll(async () => {
     browser.waitForAngularEnabled(false);
     await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.goToAddToCardMenu();
-    await productAddedModal.goToShoppingCartSummary();
-    await summaryStep.goToSignInStep();
+  });
 
-    await signInStep.login('aperdomobo@gmail.com', 'WorkshopProtractor');
+  describe('Buy a t-shirt process', () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productListPage: ProductListPage = new ProductListPage();
+    const productAddedModal: ProductAddedModal = new ProductAddedModal();
+    const summaryStep: SummaryStep = new SummaryStep();
 
-    await addressStep.goToShippingStep();
+    beforeAll(async () => {
+      await menuContentPage.goToTShirtMenu();
+      await productListPage.goToAddToCardMenu();
+      await productAddedModal.goToShoppingCartSummary();
+      await summaryStep.goToSignInStep();
+    });
 
-    await shippingStep.goToPaymentStep();
-    await paymentStep.goToBankPaymentMenu();
-    await bankPayment.goToOrderSummary();
+    describe('Login in the app process', () => {
+      const signInStep: SignInStep = new SignInStep();
 
-    await expect(orderSummary.getConfimantionText())
-      .toBe('Your order on My Store is complete.');
+      beforeAll(async () => {
+        await signInStep.login('aperdomobo@gmail.com', 'WorkshopProtractor');
+      });
+
+      describe('Select address and shipping process', () => {
+        const addressStep: AddressStep = new AddressStep();
+        const shippingStep: ShippingStep = new ShippingStep();
+
+        beforeAll(async () => {
+          await addressStep.goToShippingStep();
+          await shippingStep.goToPaymentStep();
+        });
+
+        describe('Bank Payment process', () => {
+          const paymentStep: PaymentStep = new PaymentStep();
+          const bankPayment: BankPayment = new BankPayment();
+          const orderSummary: OrderSummary = new OrderSummary();
+
+          beforeAll(async () => {
+            await paymentStep.goToBankPaymentMenu();
+            await bankPayment.goToOrderSummary();
+          });
+
+          it('then should be bought a t-shirt', async () => {
+            await expect(orderSummary.getConfimantionText())
+              .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
