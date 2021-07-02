dffa921a1cc16b2cb056d51125c8d807788a1eb4
Angie Valentina Cordoba <valentina.cordobap@perficient.com>
2021-07-02 12:32:55 -0500

    Module 20

---
+++ src/page/i-frame.page.ts
@@ -0,0 +1,17 @@
+import { $, browser, ElementFinder, promise } from 'protractor';
+
+export class IFramePage {
+  private iframe1: ElementFinder;
+
+  constructor() {
+    this.iframe1 = $('#frame1');
+  }
+
+  public setFormFrameHeight(height: number): promise.Promise<void> {
+    return browser.executeScript(`arguments[0].height = ${height};`, this.iframe1);
+  }
+
+  public async getIframeHeight(): Promise<number> {
+    return (await this.iframe1.getSize()).height;
+  }
+}
