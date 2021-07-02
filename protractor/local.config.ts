711fbf57604cd4a7b088001c959ed8529e2344b9
Angie Valentina Cordoba <valentina.cordobap@perficient.com>
2021-07-02 08:08:32 -0500

    Module 19 fixed

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,7 +3,7 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/**/locators.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   getPageTimeout: 30000,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000,
